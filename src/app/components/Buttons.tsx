9569d4232fc7b62de5eeab7e760e541a65dd9605
Hamza <qhamza4532@gmail.com>
2024-04-02 14:34:59 +0500

    Buttons

--- src/app/components/Buttons.tsx
+++ src/app/components/Buttons.tsx
@@ -4,19 +4,19 @@ function Buttons() {
   return (
     <div>
         <div className="space-x-2 ml-10">
-  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
     Blue
   </button>
-  <button className="bg-green-500 w-[10%] hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-green-500 w-[10%] hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
     Green
   </button>
   <button className="bg-white hover:bg-red-700 text-black border rounded-full w-[12%] font-bold py-2 px-4 ">
     Red
   </button>
-  <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-yellow-800 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
     Yellow
   </button>
-  <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-purple-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
     Purple
   </button>
 </div>
